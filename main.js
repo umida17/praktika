30922cfff4915098584a211c4ecab1d8df123f43
uy <a.normurzaev20@gmail.com>
2026-04-13 11:45:03 +0500

    ozgartirish kiritildi

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ fetch('https://jsonplaceholder.typicode.com/albums')
 function render(users) {
     const cards = users.map(el =>`
         <div class = "card">
-            <h2>${el.title}</h2>
-            <p>${el.url}</p>
-            <a href="#">${el.thumbnailUrl}</a>
+            <h2>${el.name}</h2>
+            <p>${el.email}</p>
+            <a href="#">${el.body}</a>
 </div>
         `).join("")
         wapper.innerHTML = cards;
