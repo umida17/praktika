fdd77c5e109e346e4d843ffb8aedcfa00db7cf02
uy <a.normurzaev20@gmail.com>
2026-04-13 11:34:05 +0500

    main js yoilmoqda function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+const wapper = document.getElementById("container");
+
+fetch('https://jsonplaceholder.typicode.com/albums')
+  .then(response => response.json())
+  .then(data => {
+    render (data)
+  })
+  .catch(error => console.error("xatolik yuz berdi", error));
+
+function render(users) {
+    const cards = users.map(el =>`
+        
+        `)
+}
